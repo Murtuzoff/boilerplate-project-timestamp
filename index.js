82c3204b814cf3c7c37fed45fd0d483d7a546abc
Ruslan Murtuzoff <50267310+Murtuzoff@users.noreply.github.com>
2024-04-20 23:49:17 +0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const app = express();
 
 const cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 }));
-app.use(express.static('public'));
+app.use(express.static('public', {
+  setHeaders: (res, path, stat) => {
+    if (path.endsWith('.css')) {
+      res.setHeader('Content-Type', 'text/css');
+    }
+  }
+}));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
